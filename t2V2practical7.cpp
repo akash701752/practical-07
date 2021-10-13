ec0e8929ba4e20e4608d98322e5f261fe351305e
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-13 09:36:59 +0530

    Create t2V2practical7.cpp

---
+++ t2V2practical7.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#define MAX 3 
+using namespace std ;
+int main()
+{
+  int arr[MAX][MAX] , i , j ;
+  cout<<"Enter Matrix ";
+  for(i=0;i<MAX;j++)
+  {
+    for(j=0;j<MAX;j++)
+    {
+      cin>>arr[i][j] ;
+    }
+  }
+  cout<<"Input Matrix is "<<endl;
+  for(i=0;i<MAX;j++)
+  {
+    for(j=0;j<MAX;j++)
+    {
+      cout<<arr[i][j] ;
+    }
+    cout<<endl ;
+  }
+  cout<<"Output Matrix is "<<endl;
+  for(i=0;i<MAX;j++)
+  {
+    for(j=0;j<MAX;j++)
+    {
+      cout<<10-arr[i][j] ;
+    }
+    cout<<endl ;
+  }
+  return 0 ;
+}
