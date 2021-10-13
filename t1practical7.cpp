63fec739cfbec1f4c4bffc16e6ffa177e75892a1
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-13 10:20:17 +0530

    Update t1practical7.cpp

--- t1practical7.cpp
+++ t1practical7.cpp
@@ -4,10 +4,10 @@ using namespace std ;
 int main()
 {
   int ch ;
-  cout<<"1. For Crrent Year "<<endl ;
-  cout<<"2. For Crrent Month "<<endl ;
-  cout<<"3. For Crrent Date "<<endl ;
-  cout<<"4. For Crrent Day "<<endl ;
+  cout<<"1. For Current Year "<<endl ;
+  cout<<"2. For Current Month "<<endl ;
+  cout<<"3. For Current Date "<<endl ;
+  cout<<"4. For Current Day "<<endl ;
   cout<<"Enter Your Choice : ";
   cin>>ch ;
   switch(ch)
@@ -18,32 +18,7 @@ int main()
       break ;
     case 3 : cout<<"Current Date is : 13" <<endl;
       break ;
-    case   : cout<<"Current Day is : Wedesday " <<endl;
-      break ;
-    default : cout<<"!!! Invalid Choice !!!"<<endl ;
-      break ;
-  }
-  return 0 ;
-}#include<iostream>
-using namespace std ;
-int main()
-{
-  int ch ;
-  cout<<"1. For Crrent Year "<<endl ;
-  cout<<"2. For Crrent Month "<<endl ;
-  cout<<"3. For Crrent Date "<<endl ;
-  cout<<"4. For Crrent Day "<<endl ;
-  cout<<"Enter Your Choice : ";
-  cin>>ch ;
-  switch(ch)
-  {
-    case 1 : cout<<"Current Year is : 2021" <<endl;
-      break ;
-    case 2 : cout<<"Current Month is : October" <<endl;
-      break ;
-    case 3 : cout<<"Current Date is : 13" <<endl;
-      break ;
-    case   : cout<<"Current Day is : Wedesday " <<endl;
+    case 4 : cout<<"Current Day is : Wedesday " <<endl;
       break ;
     default : cout<<"!!! Invalid Choice !!!"<<endl ;
       break ;
