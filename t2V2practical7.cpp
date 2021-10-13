12ce5b82f3b6b28d25bb399b94127e54dfd603ed
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-13 10:33:31 +0530

    Update t2V2practical7.cpp

--- t2V2practical7.cpp
+++ t2V2practical7.cpp
@@ -1,12 +1,12 @@
-// Task 1 Version 2  - To print Matrix in Reverse Order Using 10-A[i][j]
+// Task 2 Version 2  - To print Matrix in Reverse Order Using 10-A[i][j]
 #include<iostream>
 #define MAX 3 
 using namespace std ;
 int main()
 {
   int arr[MAX][MAX] , i , j ;
-  cout<<"Enter Matrix ";
-  for(i=0;i<MAX;j++)
+  cout<<"Enter Matrix "<<endl;
+  for(i=0;i<MAX;i++)
   {
     for(j=0;j<MAX;j++)
     {
@@ -14,20 +14,20 @@ int main()
     }
   }
   cout<<"Input Matrix is "<<endl;
-  for(i=0;i<MAX;j++)
+  for(i=0;i<MAX;i++)
   {
     for(j=0;j<MAX;j++)
     {
-      cout<<arr[i][j] ;
+      cout<<arr[i][j] << " " ;
     }
     cout<<endl ;
   }
   cout<<"Output Matrix is "<<endl;
-  for(i=0;i<MAX;j++)
+  for(i=0;i<MAX;i++)
   {
     for(j=0;j<MAX;j++)
     {
-      cout<<10-arr[i][j] ;
+      cout<<10-arr[i][j] << " " ;
     }
     cout<<endl ;
   }
