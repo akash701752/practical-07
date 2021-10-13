43e03cbe553c2f833aac68aec9098a0020086e3a
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-13 09:22:06 +0530

    Update and rename t1practical6.cpp to t1practical7.cpp

--- t1practical7.cpp
+++ t1practical7.cpp
@@ -16,7 +16,29 @@ int main()
       break ;
     case 3 : cout<<"Current Date is : 13" <<endl;
       break ;
+    default : cout<<"!!! Invalid Choice !!!"<<endl ;#include<iostream>
+using namespace std ;
+int main()
+{
+  int ch ;
+  cout<<"1. For Crrent Year "<<endl ;
+  cout<<"2. For Crrent Month "<<endl ;
+  cout<<"3. For Crrent Date "<<endl ;
+  cout<<"Enter Your Choice : ";
+  cin>>ch ;
+  switch(ch)
+  {
+    case 1 : cout<<"Current Year is : 2021" <<endl;
+      break ;
+    case 2 : cout<<"Current Month is : October" <<endl;
+      break ;
+    case 3 : cout<<"Current Date is : 13" <<endl;
+      break ;
     default : cout<<"!!! Invalid Choice !!!"<<endl ;
+      break ;
+  }
+  return 0 ;
+}
       break ;
   }
   return 0 ;
