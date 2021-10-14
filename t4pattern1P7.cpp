21d23026f1550d4d074a867659b42e00cac5a8b6
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-14 10:15:11 +0530

    Add files via upload

---
+++ t4pattern1P7.cpp
@@ -0,0 +1,42 @@
+// Task 4 To print Pattern using Array
+/*
+   *
+   * *
+   * * *
+   * * * *
+*/
+#include<iostream>
+//#define MAX 4 
+using namespace std ;
+int main()
+{
+  
+  int  i , j , m , n ;
+  cout<<"Enter Order Of Square Matrix as mXn : " ; // m=n for Square Matrix
+  cin>>m>>n ;
+  char arr[m][n] ;
+  cout<<"Output Pattern "<<endl;
+  for(i=0;i<m;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      if(j<=i)
+      {
+        arr[i][j] = '*' ;
+      }
+      else
+      {
+       arr[i][j] = ' ' ;
+      }
+    }
+  }
+  for(i=0;i<m;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      cout<<arr[i][j] ;
+    }
+    cout<<endl ;
+  }
+  return 0 ;
+}
